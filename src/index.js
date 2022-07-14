5206c4de2a9d9cb7cc6d2b24a6cfc2f30ebfdb6d
Nguyễn Thanh Nguyên <nguyenkiddo0@gmail.com>
2022-07-14 14:03:46 +0700

    add code

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import ReactDOM from "react-dom";
+import { render } from "react-dom"
+
+import "./index.css";
+import App from "./App";
+
+// v18
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+// v17
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+const root = document.getElementById('root')
+render(<App />, root);
